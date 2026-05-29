97a8d3b899cc371fab237a03c4048f1156611676
Roberto Coratti <corattiroberto@gmail.com>
2026-05-29 22:18:22 +0200

    feat: setup npm distribution and lib build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite"
 import tsConfigPaths from "vite-tsconfig-paths"
 import react from "@vitejs/plugin-react-swc"
+import dts from "vite-plugin-dts"
+import { resolve } from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [tsConfigPaths(), react()],
+	plugins: [tsConfigPaths(), react(), dts({ rollupTypes: true })],
+	build: {
+		lib: {
+			entry: resolve("src/index.ts"),
+			formats: ["es", "cjs"],
+			fileName: (format) => `index.${format === "cjs" ? "cjs" : "js"}`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+		},
+	},
 })
